36c3969b43047dfecd1857db5c8a3485b02a54fd
Pkdriller <benardwachira49@gmail.com>
2025-04-17 01:56:29 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -87,7 +87,7 @@ router.get('/', async (req, res) => {
 
                     // Send confirmation message
                     await Um4r719.sendMessage(userJid, { 
-                        text: '*Hey Dear👋*\n\n*Don’t Share Your Session ID With Anyone*\n\n*This Is Nexus-xmd🔥*\n\n*THANKS FOR USING NEXUS-XMD BOT*\n\n*CONNECT FOR UPDATES*: https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ Pk-Dreller Thanks for HansTz Tech🔥\n' 
+                        text: '*Hey Dear👋*\n\n*Don’t Share Your Session ID With Anyone*\n\n*This Is pkdriller bots session id 🔥*\n\n*THANKS FOR USING PK BOTS*\n\n*CONNECT FOR UPDATES*: https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ Pk-Driller Thanks Alot🔥\n' 
                     });
 
                     // Clean up session after use
